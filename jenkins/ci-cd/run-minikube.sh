c6d109b19898d8007bc9a88dc55e47046dedada9
Eduard Usatchev <eduard.usatchev@gmail.com>
2024-07-17 16:46:03 +0300

    debug jenkins

---
+++ jenkins/ci-cd/run-minikube.sh
@@ -0,0 +1,5 @@
+# https://medium.com/@devayanthakur/minikube-configure-jenkins-kubernetes-plugin-25eb804d0dec
+kubectl apply -f account.yaml
+kubectl get secrets
+kubectl describe secrets/jenkins-token
+cat /home/devyan/.minikube/ca.crt | base64 -w 0; echo
