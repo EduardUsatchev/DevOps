559fdeb800871537d40a5d64ba43e899ee7a80d8
Eduard Usatchev <eduard.usatchev@gmail.com>
2024-09-03 11:11:06 +0300

    add lessons

--- devops_program/l_14_15_terraform/lesson_14/devops-deploy/dev/jenkins/main.tf
+++ devops_program/l_14_15_terraform/lesson_14/devops-deploy/dev/jenkins/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQ3EGUEWMQFCB2PXD"
-  secret_key = "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"
+  access_key = "#######3"
+  secret_key = "#######"
 }
 module "jenkins" {
   source = "../../modules/jenkins"
