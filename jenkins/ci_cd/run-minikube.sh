69b7897d7067e56c5d72415c846d7b08bb61b920
Eduard Usatchev <eduard.usatchev@gmail.com>
2024-09-03 10:01:50 +0300

    add lessons

--- jenkins/ci_cd/run-minikube.sh
+++ jenkins/ci_cd/run-minikube.sh
@@ -6,3 +6,8 @@ cat /home/devyan/.minikube/ca.crt | base64 -w 0; echo
 
 kubectl apply -f jenkins/ci-cd/k8s-manifests/deployment.yaml
 kubectl port-forward service/flask-app-service 8080:80
+
+kubectl apply -f jenkins-k8s-config.yaml
+kubectl get secret $(kubectl get serviceaccount jenkins-sa -o jsonpath='{.secrets[0].name}') -o jsonpath='{.data.token}' | base64 --decode
+docker run -p 5000:80 flask-app
+doc
