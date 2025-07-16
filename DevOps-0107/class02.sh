dda0286b820bbcf5a44cf16d1f310b4ade59cffd
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-07-16 09:31:25 +0300

    Create class02.sh

---
+++ DevOps-0107/class02.sh
@@ -0,0 +1,19 @@
+docker run -d -p 80:80 nginx:alpine
+docker build -t class02-ex1 .
+docker run -p 80:80 -d
+docker build -t my_image -f Dockerfile
+docker run -d my_image
+docker logs
+docker exec -it 890 sh
+docker network create wordpress
+docker run --name wordpress-container --rm --network wordpress -e WORDPRESS_DB_HOST=mysql-container -e WORDPRESS_DB_PASSWORD=wordpress -p 8090:80 -d wordpress
+docker run --name mysql-container --rm --network wordpress -e MYSQL_ROOT_PASSWORD=wordpress -d mariadb:latest
+docker exec -it mysql-container bash / sh
+  curl http://wordpress-container
+  curl 172.20.0.2
+docker-compose up -d
+docker rm -vf $(docker ps -qa)
+docker tag my_image eduardusatchev/my_image:v1302-edi
+docker login
+docker push a18ab77c4b90/my_image:v1302-edi
+docker run -it -v /Users/e0u00jg/Desktop/DevOpsCourse/DevOps1302/lessons/lesson_09/my_docker/data:/data alpine:latest
