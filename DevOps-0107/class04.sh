b3ba64444aa8de86d45600e17538a3e000f3e589
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-07-22 14:54:55 +0300

    Update class04.sh

--- DevOps-0107/class04.sh
+++ DevOps-0107/class04.sh
@@ -3,6 +3,31 @@ https://kubernetes.io/docs/reference/kubectl/generated/kubectl_completion/
 kubectl expose deploy/hello-node --type LoadBalancer --port 80
 kubectl delete pod nginx
 kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/pod-with-service.yaml
-kubectl apply -f https://raw.githubusercontent.com/avielb/k8s-demo/refs/heads/master/guestbook/cronjob.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/guestbook/cronjob.yaml
 
+kubectl get pods 
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pv.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pvc.yaml
+kubectl describe persistentvolumeclaim/task-pv-claim
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl exec -it task-pv-pod -- bash
+cd usr/share/nginx/html/
+echo 1 > index.html
+exit
+kubectl debug node/docker-desktop -it --image=busybox -- sh
+kubectl delete -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-empty-dir.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/config-map.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/game-config.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-config-map.yaml
+kubectl exec -it pod-env-var -- bash
+cat /app/game.properties
+exit
+kubectl describe configmap game-config
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/secret.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-secret.yaml
+kubectl delete -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/statefulset.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/statefulset.yaml
+kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/daemonset.yaml
 
