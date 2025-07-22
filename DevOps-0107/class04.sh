609213778f16e2ed2e18eb9e63abb0840a55ac8a
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-07-22 20:32:51 +0300

    Update class04.sh

--- DevOps-0107/class04.sh
+++ DevOps-0107/class04.sh
@@ -50,7 +50,9 @@ kubectl exec -it task-pv-pod -- bash
 cd usr/share/nginx/html/
 echo 1 > index.html
 exit
+#Re-Run AT HOME
 kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-retain-deployment.yaml
+
 kubectl debug node/docker-desktop -it --image=busybox -- sh
 kubectl delete -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
 kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/volumes/pod-with-pvc.yaml
