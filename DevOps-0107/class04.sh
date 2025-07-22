2fbab377334a2c91e02558da9120d7f5736bc1b8
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-07-22 19:12:54 +0300

    Update class04.sh

--- DevOps-0107/class04.sh
+++ DevOps-0107/class04.sh
@@ -19,6 +19,7 @@ https://kubernetes.io/docs/reference/kubectl/generated/kubectl_completion/
 kubectl expose deploy/hello-node --type LoadBalancer --port 80
 kubectl delete pod nginx
 kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/pod-with-service.yaml
+https://crontab.guru/
 kubectl apply -f https://raw.githubusercontent.com/EduardUsatchev/k8s-demo/refs/heads/master/guestbook/cronjob.yaml
 
 kubectl get pods 
