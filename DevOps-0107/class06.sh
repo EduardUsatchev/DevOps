be113590702b51810a964450861019205b827331
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-08-19 19:16:09 +0300

    Update class06.sh

--- DevOps-0107/class06.sh
+++ DevOps-0107/class06.sh
@@ -7,21 +7,21 @@ git remote add origin https://github.com/EduardUsatchev/class6-git.git
 git branch -M main
 git push -u origin main
 touch 1.txt
- 3647  git status
- 3648  touch 2.txt
- 3649  touch 2.tx
- 3650  git status
- 3651  git add 1.txt
- 3652  git status
- 3653  git add .
- 3654  git status
- 3655  git rm --cached 1.txt
- 3656  git status
- 3657  git commit -m "initial commit"
- 3658  git add .
- 3659  git commit -m "change image version"
- 3660  git status
- 3661  git log
- 3662  ls -la
- 3663  ls -la .git
- 3664  git log
+git status
+touch 2.txt
+touch 2.tx
+git status
+git add 1.txt
+git status
+git add .
+git status
+git rm --cached 1.txt
+git status
+git commit -m "initial commit"
+git add .
+git commit -m "change image version"
+git status
+git log
+ls -la
+ls -la .git
+git log
