0baf759b7425ad088e57f71d1eba0fe37bc6b3ff
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-08-19 19:15:08 +0300

    Update class06.sh

--- DevOps-0107/class06.sh
+++ DevOps-0107/class06.sh
@@ -2,3 +2,26 @@ git init
 git rm --cached 1.txt
 git config user.name "Your Name"
 git config user.email "your_email@example.com"
+
+git remote add origin https://github.com/EduardUsatchev/class6-git.git
+git branch -M main
+git push -u origin main
+touch 1.txt
+ 3647  git status
+ 3648  touch 2.txt
+ 3649  touch 2.tx
+ 3650  git status
+ 3651  git add 1.txt
+ 3652  git status
+ 3653  git add .
+ 3654  git status
+ 3655  git rm --cached 1.txt
+ 3656  git status
+ 3657  git commit -m "initial commit"
+ 3658  git add .
+ 3659  git commit -m "change image version"
+ 3660  git status
+ 3661  git log
+ 3662  ls -la
+ 3663  ls -la .git
+ 3664  git log
