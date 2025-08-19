c14676aafe7a4c6c4b1fac65abb3e7a878062e67
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-08-19 20:19:32 +0300

    Update class06.sh

--- DevOps-0107/class06.sh
+++ DevOps-0107/class06.sh
@@ -28,3 +28,30 @@ git log
 https://github.com/EduardUsatchev/class6-git
 git checkout -b new-branch
 git checkout main
+git status
+git pull
+git log
+git checkout -b new-branch
+git status
+git checkout main
+ls -la
+git checkout new-branch
+ls -la
+touch 5.txt
+git add .
+git commit -m "feature 5"
+ls -la
+git checkout main
+ls -la
+git checkout new-branch
+ls -la
+git push
+git push --set-upstream origin new-branch
+git checkout -b clone-branch
+ll
+echo 1 > 5.txt
+git add .
+git commit -m "fix bug"
+git push
+git push --set-upstream origin clone-branch
+git log --graph --oneline --all
