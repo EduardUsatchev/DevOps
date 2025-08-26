10e603317f8dbfbe2a3f4bc9d91b08dc9af3061b
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-08-26 18:23:56 +0300

    Update class07.sh

--- DevOps-0107/class07.sh
+++ DevOps-0107/class07.sh
@@ -1,2 +1,23 @@
 # Create repository in Github for the class
 # Link to the materials: https://github.com/EduardUsatchev/DevOps/tree/main/github-actions/workflows
+# Create file : .github/workflows/01-example.yaml
+name: Run Tests
+
+on:
+  push:
+    branches:
+        - test
+  workflow_dispatch:
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    container: python:3.11
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v3
+      - name: show files
+        run: |
+          ls -ltr
+          python --version
+#####################################3
