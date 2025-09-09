3254cf81103dab7504c0204ed3cd3c285f2855b7
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-09-09 18:59:54 +0300

    Update class09.sh

--- DevOps-0107/class09.sh
+++ DevOps-0107/class09.sh
@@ -1,4 +1,5 @@
 First: git clone https://github.com/EduardUsatchev/advanced-devops.git
+cd advanced-devops/monitoring/k8s-setup
 minikube start --driver=docker
 1. kubectl apply -f monitoring/namespace.yml
 2. helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
